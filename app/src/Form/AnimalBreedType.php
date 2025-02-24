a68078348727b853dca56ac70a2897be724aaaec
F.OCT <souhail.freij@gmail.com>
2025-02-24 17:19:09 +0100

    correction Animal breed Form et affichage

--- app/src/Form/AnimalBreedType.php
+++ app/src/Form/AnimalBreedType.php
@@ -4,16 +4,18 @@ namespace App\Form;
 
 use App\Entity\AnimalBreed;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AnimalBreedType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->add('breed');
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => AnimalBreed::class,
